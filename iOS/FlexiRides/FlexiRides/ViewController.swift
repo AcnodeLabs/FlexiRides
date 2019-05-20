fd148e49d08695e58cb4ff5b74523cf1ae394618
Bilal <three6tdegree@gmail.com>
2019-05-20 10:59:12 +0500

    added ios

---
+++ iOS/FlexiRides/FlexiRides/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlexiRides
+//
+//  Created by Bilal Ahsan on 20/05/2019.
+//  Copyright © 2019 xal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
