cd8b1d22b58414d795d88965a9e8124600df271b
Bilal <three6tdegree@gmail.com>
2019-05-28 16:55:33 +0500

    AndroidStudio

---
+++ iOS/FlexiRides/FlexiRides/Logic/FlexiRidesLogic.cpp
@@ -0,0 +1,9 @@
+//
+//  FlexiRidesLogic.cpp
+//  FlexiRides
+//
+//  Created by Bilal Ahsan on 20/05/2019.
+//  Copyright © 2019 xal. All rights reserved.
+//
+
+#include "FlexiRidesLogic.hpp"
---
+++ iOS/FlexiRides/FlexiRides/Logic/FlexiRidesLogic.hpp
@@ -0,0 +1,14 @@
+//
+//  FlexiRidesLogic.hpp
+//  FlexiRides
+//
+//  Created by Bilal Ahsan on 20/05/2019.
+//  Copyright © 2019 xal. All rights reserved.
+//
+
+#ifndef FlexiRidesLogic_hpp
+#define FlexiRidesLogic_hpp
+
+#include <stdio.h>
+
+#endif /* FlexiRidesLogic_hpp */
